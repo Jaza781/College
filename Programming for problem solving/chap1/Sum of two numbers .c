486c92a41e62a66817546b2ef778df3acf4a2bf1
Jaza781 <jazaabid876@gmail.com>
2025-04-26 13:56:16 +0530

    Update Sum of two numbers .c

--- Programming for problem solving/chap1/Sum of two numbers .c
+++ Programming for problem solving/chap1/Sum of two numbers .c
@@ -1,1 +1,12 @@
+#include <stdio.h>;
+int main(){
+  float a,b;
+  printf("\nInput the first number\n");
+  scanf("%f",&a);
+  printf("\nInput the second number\n");
+  scanf("%f",&b);
+  printf("nThe sum of %f and %f is %f,a,b,a+b);
+
+  return 0;
+}
 
