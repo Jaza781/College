47cd29f3f955e965f2a36cfa8dce18039ec36caf
Jaza781 <jazaabid876@gmail.com>
2025-04-26 14:07:33 +0530

    Create Remainder_of_2nums.c

---
+++ Remainder_of_2nums.c
@@ -0,0 +1,13 @@
+//For modulus operator, only int value can be used
+#include <stdio.h>;
+int main(){
+  int a,b;
+  printf("\nInput the first number\n");
+  scanf("%d",&a);
+  printf("nInput the second number\n");
+  scanf("%d",&b);
+  if(b!=0){
+       printf("\nThe remainder when %d is divided by %d is %d\n",a,b,a%b);}
+  else{
+       printf("\nThe division is not possible\n");}
+  return 0;
