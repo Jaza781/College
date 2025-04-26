da1ddcdf03dbf9d7fd2bf9936cd04219c8a67e13
Jaza781 <jazaabid876@gmail.com>
2025-04-26 21:42:53 +0530

    Update Increment&Decrement_Operators.c

--- Programming for problem solving/chap1/Increment&Decrement_Operators.c
+++ Programming for problem solving/chap1/Increment&Decrement_Operators.c
@@ -4,3 +4,15 @@ int main() {
   int a = 10, b = 5, c;
   int *ptr =&a;
 
+//Increment/Decrement Operators
+printf("Increment/Decrement Operators:\n");
+printf("a++ = %d\n", a++);
+printf("Now a = %d\n", a);
+printf("++a = %d\n", ++a);
+printf("a-- = %d\n", a--);
+printf("Now a = %d\n", a);
+printf("--a = %d\n\n", --a);
+
+return 0;
+}
+
