419142359e65ee097bc57c9227a20c39a1ce8b3a
Jaza781 <jazaabid876@gmail.com>
2025-04-26 18:44:46 +0530

    Update Difference of 2 nums.c

--- Programming for problem solving/chap1/Difference of 2 nums.c
+++ Programming for problem solving/chap1/Difference of 2 nums.c
@@ -2,10 +2,10 @@
 int main(){
     float a,b;
     printf("\nInput the first number\n");
-    scanf("%f,&a);
+    scanf("%f",&a);
     printf("\nInput the second number\n");
     scanf("%f",&b);
-    printf("\nThe difference %f and %f is %f,a,b,a-b);
+    printf("\nThe difference %f and %f is %f",a,b,a-b);
 
    return 0;
 }
