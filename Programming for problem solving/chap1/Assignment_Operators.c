02b8236211dbf4f77ee32864c1e65a421419929f
Jaza781 <jazaabid876@gmail.com>
2025-04-26 21:29:47 +0530

    Create Assignment_Operators.c

---
+++ Programming for problem solving/chap1/Assignment_Operators.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+intmain() {
+int a = 10, b = 5,c;
+
+ // Assignment Operators
+    printf("Assignment Operators:\n");
+    c = a;
+    c += b;
+    printf("c += b: %d\n", c);
+    c -= b;
+    printf("c -= b: %d\n", c);
+    c *= b;
+    printf("c *= b: %d\n", c);
+    c /= b;
+    printf("c /= b: %d\n", c);
+    c %= b;
+    printf("c %%= b: %d\n\n", c);
+
+return 0;
+}
