1cd87ee25f9ba16e2447daa2750c84edb906d232
Jaza781 <jazaabid876@gmail.com>
2025-07-29 00:12:04 +0530

    Create Program to display pattern 2.c

---
+++ Programming for problem solving/chap1/Program to display pattern 2.c
@@ -0,0 +1,20 @@
+//Program in C to display the following patterns
+ 
+//PATTERN 2
+// *
+// *
+// *
+// *
+printf("\n");
+
+printf("Pattern 2: \n");
+for (i = 1; i <= 4; i++) {
+   for (j = 1; j <=5; j++) {
+    printf("*");
+   }
+   printf("\n");
+}
+
+
+  return 0;
+}
